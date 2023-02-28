86ead3c0f23956e98facbdc4e7d8b141bdea8c86
meshramamrapali <amrapalimeshram86@gmail.com>
2023-02-28 22:15:09 +0530

    ADDED : Line is Equal,Grater or Less Than

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -8,10 +8,20 @@ public class LineComparison {
         Integer l1 = new Integer((int) lengthOfLine1);
         Integer l2 = new Integer((int) lengthOfLine1);
 
-        if(lengthOfLine1==lengthOfLine1) {
+        if (lengthOfLine1 == lengthOfLine1) {
             System.out.println("line are equal");
-        } else if(lengthOfLine2==lengthOfLine1){
+        } else if (lengthOfLine2 == lengthOfLine1) {
             System.out.println("line are not equal");
         }
+        String str_Sample = "a";
+        System.out.println("Compare To 'a' b is : " + str_Sample.compareTo("b"));
+
+        str_Sample = "b";
+        System.out.println("Compare To 'b' a is : " + str_Sample.compareTo("a"));
+
+        str_Sample = "b";
+        System.out.println("Compare To 'b' b is : " + str_Sample.compareTo("b"));
     }
 }
+
+
