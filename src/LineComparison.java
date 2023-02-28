0905d88c6016e676a28ab1691380bbf90942e8e8
meshramamrapali <amrapalimeshram86@gmail.com>
2023-02-28 20:47:16 +0530

    ADDED : Welcome Message

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,5 +1,6 @@
-public class Main {
+public class LineComparison {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Welcome To Line Comparison");
     }
 }
