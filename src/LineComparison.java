f96f4acf65e59506f2257f48dbf1635b90f88170
meshramamrapali <amrapalimeshram86@gmail.com>
2023-02-28 22:07:19 +0530

    ADDED : Check Equality

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -2,7 +2,16 @@ public class LineComparison {
     public static void main(String[] args) {
 
         double lengthOfLine1 = 4, lengthOfLine2 = 4;
-        System.out.println("Length of line1  ");
-        System.out.println("Length of line2  ");
+        System.out.println("Length of line1 = " + lengthOfLine1);
+        System.out.println("Length of line2 = " + lengthOfLine2);
+
+        Integer l1 = new Integer((int) lengthOfLine1);
+        Integer l2 = new Integer((int) lengthOfLine1);
+
+        if(lengthOfLine1==lengthOfLine1) {
+            System.out.println("line are equal");
+        } else if(lengthOfLine2==lengthOfLine1){
+            System.out.println("line are not equal");
+        }
     }
 }
